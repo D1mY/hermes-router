afac20fa281b79e09119fe49a1ed391acbec588e
d1m <dmitriy.y@outlook.com>
2022-01-20 17:41:06 +0500

    filer works

--- hermes_galileosky_filer/src/galileosky_dump.erl
+++ hermes_galileosky_filer/src/galileosky_dump.erl
@@ -16,6 +16,8 @@
         code_change/3
         ]).
 
+-define(Q_FILER, <<"hermes_galileosky_filer">>).
+
 start_link() ->
     gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
@@ -34,23 +36,23 @@ handle_info(_Info, State) ->
 
 terminate(_, {?MODULE, Port}) ->
     gen_tcp:close(Port),
-    ok.
+    'ok'.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 %%%----------------------------------------------------------------------------
 
 rmq_connect() ->
-  %% create RabbirMQ connection for self() and spawn_link() prcss
-  Connection = intercourse(<<"hermes_galileosky_filer">>, amqp_connection:start(#amqp_params_direct{},<<"hermes_galileosky_filer">>)),
-  % ok = persistent_term:put({hermes_galileosky_broker,rabbitmq_connection},Connection),
+  %% create RabbitMQ connection
+  Connection = intercourse(?Q_FILER, amqp_connection:start(#amqp_params_direct{},?Q_FILER)),
   %% subscribe to cfg queue
   Channel = intercourse(Connection, amqp_connection:open_channel(Connection)),
+  ok = persistent_term:put({hermes_galileosky_filer,rabbitmq_channel},Channel),
   % #'exchange.declare_ok'{} = amqp_channel:call(Channel, #'exchange.declare'{exchange = <<"hermes.fanout">>, type = <<"fanout">>, passive = false, durable = true, auto_delete = false, internal = false}),
   % #'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = <<"hermes">>, durable = true}),
   % #'queue.bind_ok'{} = amqp_channel:call(Channel,#'queue.bind'{queue = <<"hermes">>, exchange = <<"hermes.fanout">>}),
-  #'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = <<"hermes_galileosky_filer">>, durable = true}),
-  #'basic.consume_ok'{consumer_tag = ConsTag} = amqp_channel:subscribe(Channel, #'basic.consume'{queue = <<"hermes_galileosky_filer">>}, self()),
+  #'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = ?Q_FILER, durable = true}),
+  #'basic.consume_ok'{consumer_tag = ConsTag} = amqp_channel:subscribe(Channel, #'basic.consume'{queue = ?Q_FILER}, self()),
   %% loading stored cfg
   % read_cfg_file(cfg_path()),
   %% bgn listen cfg queue
@@ -58,11 +60,11 @@ rmq_connect() ->
   %% ending
   amqp_channel:call(Channel, #'basic.cancel'{consumer_tag = ConsTag}),
   rabbit_log:info("Hermes Galileosky filer close channel: ~p~n", [amqp_channel:close(Channel)]),
+  persistent_term:erase({hermes_galileosky_filer,rabbitmq_channel}),
   rabbit_log:info("Hermes Galileosky filer close connection: ~p~n", [amqp_connection:close(Connection)]),
-  % persistent_term:erase({hermes_galileosky_broker,rabbitmq_connection}),
   rabbit_log:info("Hermes Galileosky filer ended",[]).
 
-%%% cfg queue loop
+%%% cfg queue loop -------------------------------------------------------------
 loop(Channel) ->
   receive
     {#'basic.deliver'{delivery_tag = DlvrTag}, Content} ->
@@ -75,28 +77,79 @@ loop(Channel) ->
 
 handle_content(Content) ->
   case Content#amqp_msg.props#'P_basic'.headers of
-    [{<<"dump">>,_,HValue}] ->
-      dump_queues(HValue); %% в HValue ждем путь сохранения файлов дампа,
+    [{<<"dump">>,_,Path}] ->
+      dump_queues(Path); %% в Path ждем путь сохранения файлов дампа,
                            %% пустой бинарь - путь по умолчанию
-    [{<<"restore">>,_,HValue}] ->
-      restore_queues(HValue); %% в HValue ждем путь файлов дампа,
+    [{<<"restore">>,_,Path}] ->
+      restore_queues(Path); %% в Path ждем путь файлов дампа,
                               %% пустой бинарь - путь по умолчанию
     _ -> 'not_valid'
   end.
 
+%%% pull galileosky messages ---------------------------------------------------
 dump_queues(Path) ->
+  Qs = rabbit_amqqueue:list_names(), %% RabbitMQ internal, returns: [{resource,<<"vhost_name">>,queue,<<"queue_name">>}]
+  handle_queues(Qs,Path).
 
-  dump_queues(QList);
-dump_queues(QList) -> 
-  [Q|QListTail] = QList,
+handle_queues([], _) -> 'ok';
+handle_queues(Qs, Path) ->
+  [{resource,VNode,queue,Q}|QsTail] = Qs,
+  case Q of
+    ?Q_FILER ->
+      'ok';
+    _ ->
+      %% (?) start supervisor with `all_significant` automatic shutdown
+      handle_queue(VNode, Q)
+  end,
+  handle_queues(QsTail, Path).
+
+%% sub to Q, open file to write, spawn dump worker
+handle_queue(VNode, Q) ->
+  Channel = persistent_term:get({hermes_galileosky_filer,rabbitmq_channel}),
   #'queue.declare_ok'{message_count = Count} = amqp_channel:call(Channel, #'queue.declare'{queue = Q, durable = true}),
-  dump_messages(Count),
-  dump_queues(QListTail).
+  case Count of
+    0 ->
+      'ok';
+    _ ->
+      File = parse_path(Path, VNode, Q),
+      #'basic.consume_ok'{consumer_tag = ConsTag} = amqp_channel:subscribe(Channel, #'basic.consume'{queue = Q}, self()), % Pid = erlang:spawn_link(?MODULE, dump_messages, [Count,Q,File])
+      DlvrTag = dump_messages(Count, File),
+      nack_msgs(Channel, DlvrTag),
+      amqp_channel:call(Channel, #'basic.cancel'{consumer_tag = ConsTag})
+  end.
 
-dump_messages(0) -> ok;
-dump_messages(Count) -> dump_messages(Count - 1).
+dump_messages(Count, File) ->
+  {ok, IoDevice} = file:open(File, [write, raw, delayed_write, binary]),
+  DlvrTag = pull(Count, IoDevice, 0),
+  file:close(IoDevice),
+  DlvrTag.
 
-restore_queues(Path) -> ok.
+pull(0,_,DlvrTag) ->
+  DlvrTag;
+pull(Count, IoDevice, DlvrTag) ->
+  receive
+    {#'basic.deliver'{delivery_tag = DlvrTag1}, Content} ->
+      handle_message(Content, IoDevice),
+      pull(Count - 1, IoDevice, DlvrTag1);
+    _ -> pull(Count, IoDevice, DlvrTag)
+  end.
+
+handle_message(Content, IoDevice) ->
+  [{<<"uid">>, _, DevUID}] = Content#amqp_msg.props#'P_basic'.headers,
+  Payload = Content#amqp_msg.payload,
+  file:write(IoDevice, erlang:term_to_binary({DevUID,Payload})).
+
+
+%%% push galileosky messages ---------------------------------------------------
+%% should act as `hermes_q_pusher`:
+%  amqp_channel:call(AMQPChannel,
+%                               #'basic.publish'{routing_key = DevUID},
+%                               #amqp_msg{props = #'P_basic'{delivery_mode = 2, headers = [{<<"uid">>, longstr, DevUID}], content_encoding = <<"base64">>}, payload = [BinData]}
+%                              )
+%% DevUID must be in header, cause Galileosky device may translate packets from another Galileosky device
+%% (!) TODO: cause input will grow up snowball-like, need to save last position
+restore_queues(Path) ->
+  todo_push.
 
 %%%-----------------------------------------------------------------------------
 %%% helpers
@@ -122,7 +175,7 @@ ack_msg(Channel, DlvrTag) ->
 nack_msgs(Channel, DlvrTag) ->
   case amqp_channel:call(Channel,#'basic.nack'{delivery_tag = DlvrTag, multiplie = true, requeue = true}) of
     ok ->
-      ok;
+      'ok';
       % loop(Channel);
     blocked ->
       timer:sleep(3000),
@@ -130,3 +183,10 @@ nack_msgs(Channel, DlvrTag) ->
     closing ->
       {ok, <<"Closing channel">>}
   end.
+
+parse_path(Path, VNode, Q) ->
+  case Path = filelib:ensure_dir(erlang:binary_to_list(Path) ++ "/") of
+    ok -> Path;
+    _ -> filename:join([filename:basedir(user_data,[]),"Hermes",erlang:node()])
+  end,
+  todo_parse.
